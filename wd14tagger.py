03e8acc9088d504a62056741143d58b98b589017
mijuku233 <89350747+mijuku233@users.noreply.github.com>
2024-01-21 16:26:17 +0800

    Update wd14tagger.py

--- wd14tagger.py
+++ wd14tagger.py
@@ -92,7 +92,7 @@ async def tag(image, model_name, threshold=0.35, character_threshold=0.85, exclu
     remove = [s.strip() for s in exclude_tags.lower().split(",")]
     all = [tag for tag in all if tag[0] not in remove]
 
-    res = ("" if trailing_comma else ", ").join((item[0].replace("(", "\\(").replace(")", "\\)") + (", " if trailing_comma else "") for item in all))
+    res = ("" if trailing_comma else ", ").join((item[0] + (", " if trailing_comma else "") for item in all))
 
     print(res)
     return res
